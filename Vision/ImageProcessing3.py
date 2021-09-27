78a8a89131fd5f743a9275ae8b3e8d4b90f07647
Alex Simpson <49216744+odddozen@users.noreply.github.com>
2021-09-28 00:36:26 +1000

    Slight bug-fix / update for aruco code
    The previous push was slightly outdated, with bugs in the import & use of aruco markers.

--- Vision/ImageProcessing3.py
+++ Vision/ImageProcessing3.py
@@ -3,6 +3,7 @@ from absl import app, flags, logging
 #import core.utils as utils
 #from core.yolov4 import filter_boxes
 import cv2
+from cv2 import aruco
 #from PIL import Image
 import numpy as np
 
@@ -34,8 +35,8 @@ def main(_argv):
     #net.load_model("uavpayloadtaq_yolov4-tiny-320-merge-opt-fp16.bin")
      
 
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-    arucoParams = cv2.aruco.DetectorParameters_create()
+    arucoDict = aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+    arucoParams = aruco.DetectorParameters_create()
     
     while True:
 
@@ -50,30 +51,31 @@ def main(_argv):
         #images_data = []
         
         
-        (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
+        (corners, ids, rejected) = aruco.detectMarkers(image_data, arucoDict,
 	parameters=arucoParams)
 
         # verify *at least* one ArUco marker was detected
         if len(corners) > 0:
-	# flatten the ArUco IDs list
-	ids = ids.flatten()
-	# loop over the detected ArUCo corners
-	for (markerCorner, markerID) in zip(corners, ids):
-		# extract the marker corners (which are always returned in
-		# top-left, top-right, bottom-right, and bottom-left order)
-		corners = markerCorner.reshape((4, 2))
-		(topLeft, topRight, bottomRight, bottomLeft) = corners
-		# convert each of the (x, y)-coordinate pairs to integers
-		topRight = (int(topRight[0]), int(topRight[1]))
-		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-		topLeft = (int(topLeft[0]), int(topLeft[1]))
+        # flatten the ArUco IDs list
+            ids = ids.flatten()
+            # loop over the detected ArUCo corners
+            print(ids)
+            for (markerCorner, markerID) in zip(corners, ids):
+                # extract the marker corners (which are always returned in
+                # top-left, top-right, bottom-right, and bottom-left order)
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-	# draw the bounding box of the ArUCo detection
-        cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-        cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-        cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-        cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            # draw the bounding box of the ArUCo detection
+                cv2.line(image_data, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(image_data, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(image_data, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(image_data, bottomLeft, topLeft, (0, 255, 0), 2)
         #for i in range(1):
         #    images_data.append(image_data)
         #images_data = np.asarray(images_data).astype(np.float32)
