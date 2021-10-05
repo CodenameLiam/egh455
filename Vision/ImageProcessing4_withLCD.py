408872656d3d59c37a1f4c94fd9d29ba7640d812
Alex Simpson <49216744+odddozen@users.noreply.github.com>
2021-10-05 18:39:16 +1000

    update to display to LCD (not able to test without enviro sensorboard)

---
+++ Vision/ImageProcessing4_withLCD.py
@@ -0,0 +1,160 @@
+
+from absl import app, flags, logging
+#import core.utils as utils
+#from core.yolov4 import filter_boxes
+import cv2
+from cv2 import aruco
+#from PIL import Image
+import numpy as np
+
+from CameraHelper2 import CameraHelper
+from time import time, sleep
+#import imutils
+#from imutils.video import FPS
+from yolov4_uavpayloadtaq import YoloV4_UAVPAYLOADTAQ as search_model
+
+import ncnn
+#from ncnn.model_zoo import get_model
+from draw_detect import draw_detection_objects
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import ST7735 as ST7735
+
+score = 0.3
+iou = 0.5
+input_size = 416
+
+# Create ST7735 LCD display class.
+disp = ST7735.ST7735(
+    port=0,
+    cs=ST7735.BG_SPI_CS_FRONT,  # BG_SPI_CSB_BACK or BG_SPI_CS_FRONT
+    dc=9,
+    backlight=19,               # 18 for back BG slot, 19 for front BG slot.
+    rotation=90,
+    spi_speed_hz=4000000
+)
+
+WIDTH = disp.width
+HEIGHT = disp.height
+
+# Initialize display.
+disp.begin()
+
+# Set up canvas and font
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+
+font_size_small = 10
+font_size_large = 20
+font = ImageFont.truetype(UserFont, font_size_large)
+smallfont = ImageFont.truetype(UserFont, font_size_small)
+x_offset = 2
+y_offset = 2
+
+
+
+def main(_argv):
+    #STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config()
+    
+    
+
+    cameraHelper = CameraHelper().start()
+    sleep(1)
+    
+    myCascade = cv2.CascadeClassifier("uavpayloadtaq_cascadeclassifier.xml")
+    
+
+    arucoDict = aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+    arucoParams = aruco.DetectorParameters_create()
+    try:
+        while True:
+
+            start_cycle = time()
+            #original_image = cameraHelper.getImage()
+            
+            #print("Image Collection: " + str(time()-start_cycle))
+            
+            #image_data = cv2.resize(original_image, (input_size, input_size))
+            #image_data = image_data / 255.        
+            image_data = cameraHelper.getImage()
+            #images_data = []
+            grey = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
+
+            
+            
+            (corners, ids, rejected) = aruco.detectMarkers(image_data, arucoDict,
+            parameters=arucoParams)
+
+            # verify *at least* one ArUco marker was detected
+            if len(corners) > 0:
+            # flatten the ArUco IDs list
+                ids = ids.flatten()
+                # loop over the detected ArUCo corners
+                print(ids)
+                for (markerCorner, markerID) in zip(corners, ids):
+                    # extract the marker corners (which are always returned in
+                    # top-left, top-right, bottom-right, and bottom-left order)
+                    corners = markerCorner.reshape((4, 2))
+                    (topLeft, topRight, bottomRight, bottomLeft) = corners
+                    # convert each of the (x, y)-coordinate pairs to integers
+                    topRight = (int(topRight[0]), int(topRight[1]))
+                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                    topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                # draw the bounding box of the ArUCo detection
+                    cv2.line(image_data, topLeft, topRight, (0, 255, 0), 2)
+                    cv2.line(image_data, topRight, bottomRight, (0, 255, 0), 2)
+                    cv2.line(image_data, bottomRight, bottomLeft, (0, 255, 0), 2)
+                    cv2.line(image_data, bottomLeft, topLeft, (0, 255, 0), 2)
+            #for i in range(1):
+            #    images_data.append(image_data)
+            #images_data = np.asarray(images_data).astype(np.float32)
+
+            #print("Image Preprocessing: " + str(time()-start_cycle))
+            
+            results = face_cascade.detectMultiScale(grey, 1.3, 5)
+
+
+            for (x,y,w,h) in results:
+                cv2.rectangle(image_data,(x,y),(x+w,y+h),(255,0,0),2)
+
+                
+            #sleep(0.05)
+            #image.show()
+            # update the FPS counter
+            #fps.update()
+            FPS = 1 / ( time() - start_cycle)
+            #print("FPS: " + str(FPS))
+            FPS_message = "FPS: " + str(FPS)
+
+            #convert to PIL
+            im_pil = Image.fromarray(image_data)
+            
+            # Display the resulting frame
+            # Resize the image
+            im_pil = im_pil.resize((WIDTH, HEIGHT))
+            
+            draw = ImageDraw.Draw(im_pil)
+            x = x_offset;
+            y = y_offset;
+            draw.text((x, y), FPS_message, font=smallfont, fill=rgb)
+            
+            #display image on lcd
+            disp.display(im_pil)
+            
+            if cv2.waitKey(1)==ord('q'):
+                break
+           
+        
+        except KeyboardInterrupt:
+             # Exit cleanly
+            sys.exit(0)
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
+
