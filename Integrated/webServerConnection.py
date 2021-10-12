33a7ecb8c2b3c2b637d064e4dd87733940f31d80
ChuWengLaw <carlos.law@outlook.com>
2021-10-12 12:56:15 +1000

    fix indentation in webServerConnection.py

--- Integrated/webServerConnection.py
+++ Integrated/webServerConnection.py
@@ -8,8 +8,8 @@ class webServerConnection:
 
         self.sio.connect('http://172.19.28.216:5000')
         print('connection established')
-        
-    @sio.event   
+
+    @sio.event
     def connect():
         self.sio.connect('http://172.19.28.216:5000')
         print('connection established')
@@ -18,16 +18,13 @@ class webServerConnection:
         print('sending message of sensor data')
         self.sio.emit('sensor_data', {'Temperature': data[0], 'Pressure': data[1], 'Humidity': data[2], 'Light': data[3], 'Oxidised': data[4], 'Reduced': data[5], 'NH3': data[6]})
 
-     @sio.event
+    @sio.event
     def vision_message(data):
         print('sending message of sensor data')
         self.sio.emit('sensor_data', {'Temperature': data[0], 'Pressure': data[1], 'Humidity': data[2], 'Light': data[3], 'Oxidised': data[4], 'Reduced': data[5], 'NH3': data[6]})
-        
+
     @sio.event
     def disconnect():
         print('disconnected from server')
-        
-
-        
 
 
